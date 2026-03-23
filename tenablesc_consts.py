4fffb499cd28062525419da29a268529b7784ed5
athreyam <athreyam@splunk.com>
2026-03-23 15:44:48 +0530

    feat: add api key auth support

--- tenablesc_consts.py
+++ tenablesc_consts.py
@@ -1,6 +1,6 @@
 # File: tenablesc_consts.py
 #
-# Copyright (c) 2017-2025 Splunk Inc.
+# Copyright (c) 2017-2026 Splunk Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -27,12 +27,10 @@ SCAN_DELAY = 3
 PAGE_SIZE = 100
 
 ACCESS_KEY = "access_key"
-SECRET_KEY = "secret_key"
+SECRET_KEY = "secret_key"  # pragma: allowlist secret
 
 TENABLE_ERR_CODE_UNAVAILABLE = "Error code unavailable"
 TENABLE_ERR_MSG_UNAVAILABLE = "Error message unavailable. Please check the asset configuration and|or action parameters."
 TENABLE_ERR_INVALID_JSON = 'Error: Invalid JSON format in the "{param}".'
 TENABLE_ERR_INVALID_INT = 'Please provide a valid {msg} integer value in the "{param}"'
-TENABLE_ERR_MISSING_CREDENTIALS = (
-    "Please provide either API keys (access_key and secret_key) or username and password"
-)
+TENABLE_ERR_MISSING_CREDENTIALS = "Please provide either API keys (access_key and secret_key) or username and password"
